e598d382d519e28cc4438f9b0b60f66aae29a502
Kingpin2557 <lode.snaet@student.howest.be>
2026-03-19 12:09:49 +0100

    Add multiple modal triggers, dialog element, showModal handler, and popover CSS

--- dialog-popover/exercise/src/scripts/main.js
+++ dialog-popover/exercise/src/scripts/main.js
@@ -1,3 +1,24 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
 /* showModal, closeModal is beschikbaar op dialog elementen */
+const arrModalTrigger = document.querySelectorAll("[data-model-target]");
+
+arrModalTrigger.forEach((modalTrigger) => {
+  modalTrigger.addEventListener("click", (e) => {
+    const targetName = e.currentTarget.dataset.modelTarget;
+    const dialog = document.querySelector(`[data-model="${targetName}"]`);
+
+    if (dialog) {
+      dialog.showModal();
+
+      const closeButton = dialog.querySelector("button");
+
+      function closedDialog() {
+        dialog.close();
+      }
+
+      closeButton.addEventListener("click", () => closedDialog());
+      closeButton.removeEventListener("click", () => closedDialog());
+    }
+  });
+});
